049f4e880e125846ee33aed0be38cf3d45f19fc9
matteo-didone <matteo.dido9@gmail.com>
2023-06-13 17:16:25 +0200

    Adding comments to the html

--- script.js
+++ script.js
@@ -4,8 +4,7 @@
 const { createApp } = Vue;
 
 createApp({
-
-    // Status of the app (data) 
+    // Status of the app (data)
     data() {
         return {
             // Array of emails to be displayed on the page
@@ -13,43 +12,23 @@ createApp({
         };
     },
 
-
-    computed: {
-        allEmailAddressesGenerated() {
-            return this.emailAddressesList.length === 10;
-        },
-    },
-
-
     methods: {
-        // Get 10 email addresses from the API 
-        getEmailAddresses() {
-
-            // Loop 10 times
+        // Get 10 email addresses from the API
+        async getEmailAddresses() {
             for (let i = 0; i < 10; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                    
-                    // Push the email address into the array
-                    this.emailAddressesList.push(response.data.response);
-
-                    // Set the flag to true
-                    this.allEmailAddressesGenerated = true;
-
-                    if (this.allEmailAddressesGenerated) {
-                        // Display the email addresses on the page
-                        this.emailAddressesList.forEach((email) => {
-                            document.getElementById('email-addresses-list').innerHTML += `<li>${email}</li>`;
-                        });
-                    }
-                });
+                const response = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail');
+                this.emailAddressesList.push(response.data.response);
             }
 
-        }
+            // Display the email addresses on the page
+            this.emailAddressesList.forEach((email) => {
+                document.getElementById('email-addresses-list').innerHTML += `<li>${email}</li>`;
+            });
+        },
     },
 
     // When the app is created, but it hasn't been mounted yet, get the email addresses
     created() {
         this.getEmailAddresses();
     },
-
 }).mount('#app');
