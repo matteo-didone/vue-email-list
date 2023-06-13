765f5676c99c0acbe6c5994e072381790ffc09cd
matteo-didone <matteo.dido9@gmail.com>
2023-06-13 15:23:25 +0200

    Creating basic structure of the project

---
+++ script.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+createApp({
+
+    // Status of the app (data) 
+    data() {
+        return {
+
+        }
+    },
+
+    // Methods of the app
+    methods() {
+
+    },
+
+}).mount('#app');
